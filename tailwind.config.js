8c288adfbc5a186ae7396654053f33bb7a838508
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-09-02 15:40:24 +0300

    add colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'back-primary': '#1E1C',
+        'back-secondary': '#312F',
+        'back-third': '#514E',
+        'font-primary': 'white',
+        'nav-active': '#32B980'
+      }
+    }
+  },
+  plugins: []
 };
